c7064cec8196d95a9d08a519321977d190e03783
alaina26 <52102744+alaina26@users.noreply.github.com>
2019-07-11 15:40:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+// create a data storage for whatsapp
+
+
+let contact1 = {
+    name: "Saqib Sid",
+    number: "03009256560",
+    lastSeen: "2pm",
+    message: "Hey! Whats'up?"
+}
+let contact2 = {
+    name: "Mohsin Sid",
+    number: "0300944560",
+    lastSeen: "8am",
+    message: "Hey! Whats'up?"
+}
+let contact3 = {
+    name: "Shahrukh Sid",
+    number: "030098787",
+    lastSeen: "Yesterday",
+    message: "Hey! Whats'up?"
+}
+let contact4 = {
+    name: "Abdul Moiz",
+    number: "03009255464",
+    lastSeen: "3pm",
+    message: "Hey! Whats'up?"
+}
+let contacts =[
+    {
+        name: "Saqib Sid",
+        number: "03009256560",
+        lastSeen: "2pm",
+        message: "Hey! Whats'up?"
+    },
+    {
+        name: "Mohsin Sid",
+        number: "0300944560",
+        lastSeen: "8am",
+        message: "Hey! Whats'up?"
+    },
+     {
+        name: "Shahrukh Sid",
+        number: "030098787",
+        lastSeen: "Yesterday",
+        message: "Hey! Whats'up?"
+     },
+     {
+        name: "Abdul Moiz",
+        number: "03009255464",
+        lastSeen: "3pm",
+        message: "Hey! Whats'up?"
+     }
+
+    ]
